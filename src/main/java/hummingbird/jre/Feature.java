34dd47c4849fdc050ee8c2ae2bfe196a345fd659
Aaron Brayall <abrayall@yahoo.com>
2017-08-31 10:03:03 -0400

    updating feature definitions with names and some more file mappings

--- src/main/java/hummingbird/jre/Feature.java
+++ src/main/java/hummingbird/jre/Feature.java
@@ -4,50 +4,94 @@ import javax.util.List;
 import static javax.util.List.*;
 
 public class Feature {
-	public List<String> LANG = list();
-	public List<String> UTIL = list();
-	public List<String> JDBC = list();
-	public List<String> NIO = list();
-	public List<String> XML = list();
-	public List<String> WEBSERVICES = list();
-	public List<String> AWT = list();
-	public List<String> SWT = list();
-	public List<String> BEANS = list();
-	public List<String> CORBA = list();
-	public List<String> IMAGE = list();
-	public List<String> JMX = list();
-	public List<String> JNDI = list();
-	public List<String> MEDIA = list();
-	public List<String> NET = list();
-	public List<String> BCEL = list();
-	public List<String> XALAN = list();
-	public List<String> XERCES = list();
-	public List<String> XPATH = list();
-	public List<String> GLASSFISH = list();
 	
-	public List<String> RMI = list("bin/rmid", "bin/rmiregistry", "bin/tnameserv", "bin/orbd", "bin/servertool", "bin/policytool");
-	public List<String> KERBEROS = list("bin/keytool", "bin/kinit", "bin/klist", "bin/ktab");
+	public static Feature LANG = new Feature("lang", list());
+	public static Feature UTIL = new Feature("util", list());
+	public static Feature JDBC = new Feature("jdbc", list());
+	public static Feature NIO = new Feature("nio", list());
+	public static Feature XML = new Feature("xml", list());
+	public static Feature WEBSERVICES = new Feature("webservices", list());
+	public static Feature AWT = new Feature("awt", list());
+	public static Feature SWT = new Feature("swt", list());
+	public static Feature BEANS = new Feature("beans", list());
+	public static Feature CORBA = new Feature("corba", list());
+	public static Feature IMAGE = new Feature("image", list());
+	public static Feature JMX = new Feature("jmx", list());
+	public static Feature JNDI = new Feature("jndi", list());
+	public static Feature MEDIA = new Feature("media", list());
+	public static Feature NET = new Feature("net", list());
+	public static Feature BCEL = new Feature("bcel", list());
+	public static Feature XALAN = new Feature("xalan", list());
+	public static Feature XERCES = new Feature("xerces", list());
+	public static Feature XPATH = new Feature("xpath", list());
+	public static Feature GLASSFISH = new Feature("glassfish", list());
 	
+	public static Feature APPLET = new Feature("applet", list());
+	public static Feature TIME = new Feature("time", list());
+	public static Feature SECURITY = new Feature("security", list());
+	public static Feature MANAGEMENT = new Feature("management", list());
+	public static Feature ENCRYPTION = new Feature("encryption", list());
+	public static Feature NASHORN = new Feature("nashorn", list());
 	
-	public List<String> APPLET = list();
-	public List<String> TIME = list();
-	public List<String> SECURITY = list();
-	public List<String> MANAGEMENT = list();
-	public List<String> ENCRYPTION = list();
+
+	public static Feature RMI = new Feature("rmi", list("bin/rmid", "bin/rmiregistry", "bin/tnameserv", "bin/orbd", "bin/servertool", "bin/policytool"));
+	public static Feature KERBEROS = new Feature("kerberos", list("bin/keytool", "bin/kinit", "bin/klist", "bin/ktab"));
+
+	public static Feature JAVAFX = new Feature("javafx",
+		list("lib/ant-javafx.jar", "lib/javafx.properties", "lib/jfxswt.jar"),
+		list("decora-sse", "decora_sse", "fxplugins", "glass", "glib-lite", "gstreamer-lite", "javafx-font", "javafx_font_t2k", "javafx-iio", "jfxmedia", "jfxwebkit", "prism_common", "prism-d3d", "prism_es2", "prism_sw", "javafx_font_freetype", "javafx_font_pango")
+	);
 	
-	public List<String> FX = list("lib/ant-javafx.jar", "lib/javafx.properties", "lib/jfxswt.jar").appendAll(libs("decora-sse", "decora_sse", "fxplugins", "glass", "glib-lite", "gstreamer-lite", "javafx-font", "javafx_font_t2k", "javafx-iio", "jfxmedia", "jfxwebkit", "prism_common", "prism-d3d", "prism_es2", "prism_sw", "javafx_font_freetype", "javafx_font_pango"));
-	public List<String> WEBSTART = list("bin/javaws", "lib/javaws.jar");
-	public List<String> JFR = list("lib/jfr", "lib/jfr.jar");
+	public static Feature WEBSTART = new Feature("webstart", list("bin/javaws", "lib/javaws.jar"));
+	public static Feature JFR = new Feature("jfr", list("lib/jfr", "lib/jfr.jar"));
 	
-	public List<String> NASHORN = list();
+	public static List<Feature> FEATURES = list(JAVAFX, WEBSTART, JFR);
 	
-	
-	public List<String> lib(String name) {
-		return list("bin/" + name + ".dll", "lib/" + name + "dylib", "lib/i386/" + name + ".so", "lib/amd64/" + name + ".so");
+	public static List<Feature> features(String... names) {
+		return features(list(names));
+	}
+
+	public static List<Feature> features(List<String> names) {
+		return FEATURES.filter(feature -> names.contains(feature.name));
 	}
 	
-	public List<String> libs(String... names) {
-		return list(names).flatMap(name -> lib(name));
+	protected String name;
+	protected List<String> files = list();
+	protected List<String> libraries = list();
+	protected List<String> classes = list();
+	
+	public Feature(String name, List<String> files) {
+		this(name, files, list());
+	}
+	
+	public Feature(String name, List<String> files, List<String> libraries) {
+		this(name, files, libraries, list());
+	}
+	
+	public Feature(String name, List<String> files, List<String> libraries, List<String> classes) {
+		this.name = name;
+		this.files = files;
+		this.classes = classes;
+		this.libraries = libraries.flatMap(library -> list("bin/" + library + ".dll", "lib/" + library + "dylib", "lib/i386/" + library + ".so", "lib/amd64/" + library + ".so"));
+	}
+	
+	public boolean equals(Object object) {
+		if (Feature.class.isInstance(object))
+			return this.equals((Feature)object);
+		
+		return false;
+	}
+	
+	public boolean contains(String file) {
+		return this.files.contains(file) || this.libraries.contains(file);
+	}
+	
+	public boolean equals(Feature feature) {
+		return feature.name.equals(this.name);
+	}
+	
+	public String toString() {
+		return "feature: [name=" + this.name + "]\n\t" + "files: " + this.files + "\n\t" + "libraries: " + this.libraries + "\n\t" + "classes: " + this.classes + "\n";
 	}
 	
 	// awt.dll - awt
